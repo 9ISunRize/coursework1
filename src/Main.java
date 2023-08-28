9249916a1c3721813d2ef37cb1121f25f3fad421
Диана <dianamingazova162@gmail.com>
2023-08-28 21:45:25 +0300

    Выполнила курсовую работу

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
             new Employee("Петров Петр Петрович", 2, 20000),
             new Employee("Сидоров Сидр Сидорович", 3, 30000),
             new Employee("Михайлов Михаил Михайлович", 4, 40000),
-            new Employee("Альбертов Альберт Фльбертович", 5, 50000),
-            new Employee("Арсланов Арслан Аарсланович", 1, 10000),
-            new Employee("Розова Розетта Руслапновна", 1, 10000),
+            new Employee("Альбертов Альберт Фльбертович", 5, 51000),
+            new Employee("Арсланов Арслан Аарсланович", 1, 11000),
+            new Employee("Розова Розетта Руслапновна", 1, 12000),
             new Employee("Мухаметова Алия Альбертовнач", 3, 30000),
             new Employee("Давыдова Лариса Владимировна", 4, 40000),
             new Employee("Краснова Екатерина Дмитревна", 5, 50000),
@@ -15,16 +15,28 @@ public class Main {
     };
 
     public static void main(String[] args) {
-
+        printEmployees();
+        printSeparator();
+        System.out.println("Сумма всех зарплат: "+calculateTotalSalary());
+        printSeparator();
+        System.out.println("Сотрудник с минимальной зарплатой: "+ FindAnEmployeeWithAMinSalary());
+        printSeparator();
+        System.out.println("Сотрудник с максимальной зарплатой:"+ FindAnEmployeeWithAMaxSalary());
+        printSeparator();
+        System.out.println("средняя зарплата: "+ calculateTheAverageValueOfSalaries());
+        printSeparator();
+        printFullName ();
     }
-
+public static void printSeparator(){
+    System.out.println("___________________________________________________________________________________________________________________________________");
+}
     public static void printEmployees() {
         for (Employee employee : employees) {
             System.out.println(employee);
         }
     }
 
-    public static int calculateTotalSalary() {
+    public static float calculateTotalSalary() {
         int sumSalary = 0;
         for (Employee employee : employees) {
             sumSalary += employee.getSalary();
